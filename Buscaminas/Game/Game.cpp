0bc1cc797912e035cd8ce89f851c3110f20df3e3
Adidier <adidier.perez@gmail.com>
2022-02-16 08:42:38 -0600

    New structure of project

--- Buscaminas/Game/Game.cpp
+++ Buscaminas/Game/Game.cpp
@@ -1,4 +1,4 @@
-// Buscaminas.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Game.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include <iostream>
